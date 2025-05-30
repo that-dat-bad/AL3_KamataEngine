164dad03c7a53f454f32e8ee09b3cc7491d240b9
Takanaga Daiki <k024g0112@g.neec.ac.jp>
2025-05-30 15:42:22 +0900

    正しく動作するよう修正

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,6 +1,6 @@
 #include "KamataEngine.h"
 #include <Windows.h>
-
+#include"GameScene.h"
 
 using namespace KamataEngine;
 
@@ -9,6 +9,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// エンジンの初期化
 	KamataEngine::Initialize(L"LE2B_14_タカナガ_ダイキ_AL3");
+	//DirectXCommonインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	
+	// ゲームシーンのインスタンス生成
+	GameScene* gameScene = new GameScene();
+
+	// ゲームシーンの初期化
+	gameScene->Initialize();
 
 	// メインループ
 	while (true) {
@@ -17,8 +26,26 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		if (KamataEngine::Update()) {
 			break;
 		}
-	}
 
+		// ゲームシーンの更新
+		gameScene->Update();
+
+		//描画開始
+		dxCommon->PreDraw();
+
+
+
+		// ゲームシーンの描画
+		gameScene->Draw();
+
+
+		//描画終了
+		dxCommon->PostDraw();
+
+	}
+	delete gameScene;
+	// ゲームシーンの終了処理
+	gameScene = nullptr;
 	//エンジンの終了
 	KamataEngine::Finalize();
 	return 0;
