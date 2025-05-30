3d92a2ec800f66fe65a96733a7975589e8ad9fbe
Takanaga Daiki <k024g0112@g.neec.ac.jp>
2025-05-30 15:35:17 +0900

    修正1

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,7 +1,25 @@
+#include "KamataEngine.h"
 #include <Windows.h>
 
+
+using namespace KamataEngine;
+
 // Windowsアプリでのエントリーポイント(main関数)
-int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
-	
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	// エンジンの初期化
+	KamataEngine::Initialize(L"LE2B_14_タカナガ_ダイキ_AL3");
+
+	// メインループ
+	while (true) {
+
+		// エンジンの更新
+		if (KamataEngine::Update()) {
+			break;
+		}
+	}
+
+	//エンジンの終了
+	KamataEngine::Finalize();
 	return 0;
 }
