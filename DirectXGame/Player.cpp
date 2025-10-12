dfc415feade7b09bb17f7a9eb8db3e9ca8e4f262
Sedra -GAME <gamer.sedra@gmail.com>
2025-10-12 16:56:46 +0900

    とりあえず動くように

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -1,8 +1,8 @@
 #include "Player.h"
-
+#include <cassert>
 using namespace KamataEngine;
 
-void Player::Initialize(KamataEngine::Model* model, uint32_t textureHandle,Camera* camera) {
+void Player::Initialize(KamataEngine::Model* model, uint32_t textureHandle, Camera* camera) {
 	assert(model);
 	model_ = model;
 	textureHandle_ = textureHandle;
@@ -10,11 +10,9 @@ void Player::Initialize(KamataEngine::Model* model, uint32_t textureHandle,Camer
 	worldTransform_.Initialize();
 }
 
-void Player::Update() { 
-	worldTransform_.TransferMatrix(); }
+void Player::Update() { worldTransform_.TransferMatrix(); }
 
-void Player::Draw() 
-{ 
+void Player::Draw() {
 	KamataEngine::DirectXCommon* dxCommon = KamataEngine::DirectXCommon::GetInstance();
 	KamataEngine::Model::PreDraw(dxCommon->GetCommandList());
 	model_->Draw(worldTransform_, *camera_, textureHandle_);
